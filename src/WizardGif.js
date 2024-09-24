a4efdabb7c08942d8f4b4fd30037e2f50a5b7560
therazasayyed <157574214+therazasayyed@users.noreply.github.com>
2024-09-24 19:26:19 +0530

    Update WizardGif.js

--- src/WizardGif.js
+++ src/WizardGif.js
@@ -3,16 +3,14 @@ import React from 'react';
 const WizardGif = () => {
     const gifUrl = 'https://www.wizard.financial/static/media/wizaart-img.56787174.gif';
 
+
+
     return (
-        <div style={{ textAlign: 'center', marginBottom: '50px', marginTop: '10px', backgroundColor: '#FFFFF' }}>
+        <div className='wizard-container'>
             <img
                 src={gifUrl}
                 alt="Wizard"
-                style={{
-                    width: '742px',
-                    height: '611px',
-                    aspectRatio: 'auto 742/611'
-                }}
+                className='wizard-image'
             />
         </div>
     );
